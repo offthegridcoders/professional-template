9932bcc7bd8d3394de9919ab7fdbfd8627fe7329
Stephen Young <steveyoung359@gmail.com>
2015-12-06 14:27:11 -0600

    added forms and input animations

--- src/js/inits.js
+++ src/js/inits.js
@@ -19,4 +19,30 @@ $(function() {
       navbar.element.removeClass(navbar.secondClass);
     }
   }
+
+  function toggleClass(elm, class1, class2) {
+    if (elm.hasClass(class1)) {
+      elm.addClass(class2);
+      elm.removeClass(class1);
+    } else {
+      elm.addClass(class1);
+      elm.removeClass(class2);
+    }
+  }
+
+  $('.animate-label > input').keyup(function() {
+    var curLabel = $(this).parent();
+    if ($(this)[0].value.length != 0) {
+      if (!curLabel.hasClass('lbl-full')) toggleClass(curLabel, 'lbl-empty', 'lbl-full');
+    } else {
+      if (!curLabel.hasClass('lbl-empty')) toggleClass(curLabel, 'lbl-empty', 'lbl-full');
+    }
+  });
+
+  $('.animate-label > .fa-times').click(function() {
+    var curLabel = $(this).next();
+    console.log(curLabel);
+    curLabel.val('');
+    toggleClass($(this).parent(), 'lbl-empty', 'lbl-full');;
+  });
 });
